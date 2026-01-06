41d274aa23750fe371eda57a789af2134345739e
sbottingota <saku.bottingota@gmail.com>
2026-01-06 14:39:40 +0000

    Optimized q3a

--- q3.rs
+++ q3.rs
@@ -1,6 +1,16 @@
 use std::io;
+use memoize::memoize;
 
-fn is_raindrop(mut n: u64) -> bool {
+#[memoize]
+fn count_unordered_digits(n: u64) -> u32 {
+    if n / 10 == 0 {
+        return 0;
+    }
+
+    let unordered = if (n / 10) % 10 > n % 10 { 1 } else { 0 };
+    unordered + count_unordered_digits(n / 10)
+
+    /*
     let mut unordered_digits = 0_u32;
 
     while n > 0 {
@@ -11,14 +21,16 @@ fn is_raindrop(mut n: u64) -> bool {
         n /= 10;
     }
 
-    unordered_digits == 1
+    unordered_digits
+    */
 }
+
 
 fn count_raindrops_to(n: u64) -> u64 {
     let mut count = 0;
 
     for i in 1..n {
-        if is_raindrop(i) {
+        if count_unordered_digits(i) == 1 {
             count += 1;
         }
     }
